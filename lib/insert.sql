a89c8959b386c0e143cfc9b0f06c1b9689fe1fc5
barsu5489 <ebarsulai@gmail.com>
2023-02-15 11:52:32 +0300

    insert into table

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, sex)
+ VALUES 
+('Mr.Chocolate','M'),
+('Rowdy', 'M'),
+('Tabitha', 'F'),
+('Sergent Brown', 'M'),
+('Melissa', 'F'),
+('Grinch', 'M'),
+('Wendy', 'F'),
+(NULL,NULL);
